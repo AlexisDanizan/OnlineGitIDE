e2bf89bd74affd65307971862255873874067d32
p1317074 <p1317074@Bigglesworth>
2016-10-19 18:04:31 +0200

    Ajout des classes DAO, modification classe USer metier, ajout classe Fichier + config persistence.xml (avec ajout d?pendaces maven)

---
+++ src/main/java/API/DAO/FichierUtiliseDAO.java
@@ -0,0 +1,57 @@
+package API.DAO;
+
+import Model.FichierUtilise;
+import Model.User;
+import org.eclipse.jgit.lib.ObjectId;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+import java.util.List;
+
+/**
+ * Created by p1317074 on 19/10/16.
+ */
+public class FichierUtiliseDAO extends DAO {
+
+    public FichierUtiliseDAO(EntityManager em) {
+        super(em);
+    }
+
+    public FichierUtilise getFichierUtiliseByIdAndUser(User user, ObjectId idFichier) {
+        FichierUtilise fichier;
+        String contenu;
+        Query query = em.createNamedQuery("FichierUtilise.findByIdAndUser", FichierUtilise.class);
+        query.setParameter("user", user.getMail());
+        query.setParameter("idFichier", idFichier.getName());
+        try {
+            contenu = (String) query.getSingleResult();
+        } catch(NoResultException e) {
+            contenu = null;
+        }
+
+        fichier = new FichierUtilise(idFichier, user, contenu);
+
+        return fichier;
+
+    }
+
+
+
+    public FichierUtilise createOrUpdate(User user, ObjectId id, String contenu) {
+        FichierUtilise fichier = em.find(FichierUtilise.class, id);
+        if (fichier == null) {
+            if (id != ObjectId.zeroId()) {
+                fichier = new FichierUtilise(id, user, contenu);
+            }
+            else {
+                throw new IllegalArgumentException("Id = zeroId");
+            }
+        }
+        em.persist(fichier);
+        return fichier;
+    }
+
+
+
+}
---
+++ src/main/java/Model/FichierUtilise.java
@@ -0,0 +1,60 @@
+package Model;
+
+import javax.persistence.*;
+import org.eclipse.jgit.lib.ObjectId;
+
+import java.io.Serializable;
+
+
+/**
+ * Created by p1317074 on 19/10/16.
+ */
+
+@Entity
+@NamedQuery(name="FichierUtilise.findByIdAndUser", query="SELECT f from FichierUtilise f WHERE f.user = :user AND f.idFichier = :idFichier")
+public class FichierUtilise implements Serializable {
+
+
+    @Column(name = "contenu")
+    @Lob
+    private String contenu;
+
+    @Column(name="idFichier")
+    @Id
+    private ObjectId idFichier;
+
+    @Column(name = "user")
+    @OneToMany
+    @JoinColumn(name = "mail")
+    private User user;
+
+    public FichierUtilise(ObjectId idFichier, User user, String contenu) {
+        this.contenu = contenu;
+        this.idFichier = idFichier;
+        this.user = user;
+    }
+
+    public String getContenu() {
+        return contenu;
+    }
+
+    public void setContenu(String contenu) {
+        this.contenu = contenu;
+    }
+
+    public ObjectId getIdFichier() {
+        return idFichier;
+    }
+
+    public void setId(ObjectId id) {
+        this.idFichier = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
