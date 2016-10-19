ba4cb3e84808e0eebbfd65323cdd8b87e2800f33
p1307873 <p1307873@localhost>
2016-10-19 10:53:49 +0200

    Ajout du service arborescence

---
+++ src/main/java/API/FileTreeController.java
@@ -0,0 +1,9 @@
+package API;
+
+/**
+ * Created by Alexis on 19/10/2016.
+ */
+
+
+public class FileTreeController {
+}
---
+++ src/main/java/Model/User.java
@@ -0,0 +1,32 @@
+package Model;
+
+import javax.persistence.*;
+
+/**
+ * Created by Alexis on 17/10/2016.
+ */
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "pseudo")
+    private String pseudo;
+
+    @Column(name = "mail")
+    private String mail;
+
+    public User(String pseudo, String mail){
+        this.pseudo = pseudo;
+        this.mail = mail;
+
+    }
+
+    @Override
+    public String toString(){
+        return String.format("User[id=%d, pseudo='%s', mail='%s']",
+                id, pseudo,mail);
+    }
+}
