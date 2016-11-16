3bb05d013d9d9f0583875ae4db5018a2d30cbf05
Anthony Brage <anthony.brage@etu.univ-lyon1.f>
2016-11-16 17:31:32 +0100

    Correction des erreurs en cours 14/35

--- API/src/main/java/Service/UserService.java
+++ API/src/main/java/Service/UserService.java
@@ -1,6 +1,7 @@
 package Service;
 
 import Model.User;
+import Util.DataException;
 
 import java.util.List;
 
@@ -8,8 +9,8 @@ import java.util.List;
  * Created by amaia.nazabal on 10/19/16.
  */
 public interface UserService {
-    boolean addEntity(String pseudo, String mail, String hashkey);
-    User getEntityByMail(String mail);
-    List getEntityList();
-    boolean deleteEntity(String mail);
+    Long addEntity(String pseudo, String mail, String hashkey) throws DataException;
+    User getEntityByMail(String mail) throws DataException;
+    List getEntityList() throws Exception;
+    boolean deleteEntity(String mail) throws Exception;
 }
