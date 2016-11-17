f0a9be684bec590e6df252b0e567824ed7467135
p1513923 <p1513923@localhost>
2016-11-17 16:19:12 +0100

    COMPILE v1

--- API/src/main/java/Util/Compile.java
+++ API/src/main/java/Util/Compile.java
@@ -1,10 +1,9 @@
 package Util;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
-import static Util.Constantes.*;
 
 /**
  * Created by Mahmoud on 15/11/2016.
@@ -12,35 +11,61 @@ import static Util.Constantes.*;
 public class Compile {
 
 
-    public String executeCommpilation() throws InterruptedException, IOException {
-        String result = new String("");
-        String line = new String("");
-        Process p1 = null;
-        Runtime rt = Runtime.getRuntime();
+    public String executeCompilation() throws InterruptedException, IOException {
 
-        // Phase 1 : clone
-        // parameters :
-        p1 = rt.exec("./clone.sh "+"mahmoud appTest user");
-        p1.waitFor();
-        //Phase 2 : compilation
-        // parameters :
-        p1 = rt.exec("./compileJava.sh user appTest");
-        p1.waitFor();
+
+        this.cloneStage("mahmoud", "appTest", "user"); // CLONE
+        this.compileStage("user", "appTest"); //COMPILATION
+        String result = this.resultStage("user"); //GET RESULT
+        this.cleanStage("user"); //CLEAN
+
+        return result;
+    }
+
+    public String resultStage(String userName) throws FileNotFoundException, IOException {
+        String result = new String();
+        String line = new String();
         BufferedReader in;
-        in = new BufferedReader(new FileReader("/Users/Mahmoud/Desktop/Multimif/testCompile/appTest/testCompile/results/user.txt"));
+
+        in = new BufferedReader(new FileReader("/Users/Mahmoud/Desktop/Multimif/testCompile/appTest/testCompile/results/" + userName + ".txt"));
         result = in.readLine();
+        line = "";
+        result = "";
         while (true) {
 
             line = in.readLine();
-            if(line == null) break;
-            result+=line;
-            result+=" \n";
+            if (line == null) {
+                break;
+            }
+            result += line;
+            result += " \n";
+
         }
-        System.out.println("result : "+result );
-        //Phase 3 : Clean
-        p1 = rt.exec("./clean.sh user");
-        p1.waitFor();
+
+        //System.out.println("result : " + result);
+
         return result;
+    }
+
+    public void compileStage(String userName, String projectName) throws IOException, InterruptedException {
+        Process p1;
+        Runtime rt = Runtime.getRuntime();
+        p1 = rt.exec("./compileJava.sh " + userName + " " + projectName);
+        p1.waitFor();
+    }
+
+    public void cleanStage(String userName) throws IOException, InterruptedException {
+        Process p1;
+        Runtime rt = Runtime.getRuntime();
+        p1 = rt.exec("./clean.sh " + userName);
+        p1.waitFor();
+    }
+
+    public void cloneStage(String propProject, String projectName, String userName) throws IOException, InterruptedException {
+        Process p1;
+        Runtime rt = Runtime.getRuntime();
+        p1 = rt.exec("./clone.sh " + propProject + " " + projectName + " " + userName);
+        p1.waitFor();
     }
 
 
