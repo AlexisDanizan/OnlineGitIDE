292391d653e49b412bb1fedecda2d1ab9fe02df4
Monir Hadji <Monir Hadji@localhost>
2016-11-22 18:38:34 +0100

    correction unit test

--- API/src/main/java/Git/Constantes.java
+++ API/src/main/java/Git/Constantes.java
@@ -10,12 +10,7 @@ public class Constantes {
 
     public static final String REPOPATH = "repositories/";
 
-    // FIXME: obligé de mettre le chemin absolue pour la création d'un depot,
-    // ça ne peut pas etre un chemin relatif
-    // donc il faut modifier ce chemin pour y mettre le votre
-    // et il faudra mettre le bon chemin lorsqu'on deploiera sur la VM
-    //public static final String REPO_FULLPATH = "/Users/amaia.nazabal/test" + REPOPATH;
-    public static final String REPO_FULLPATH = "/home/hadjiszs/cours/MIF10-MULTIMIF/" + REPOPATH;
+    public static final String REPO_FULLPATH = REPOPATH;
 
     public static final String APPLICATION_JSON_UTF8 = "application/json;charset=UTF-8";
 
