31a82aa9b8b854872718bbe5397ed96e8ed95714
Monir Hadji <Monir Hadji@localhost>
2016-11-25 19:06:52 +0100

    refactor compilercontroller

--- API/src/main/java/com/multimif/controller/CompileController.java
+++ API/src/main/java/com/multimif/controller/CompileController.java
@@ -1,41 +1,40 @@
 package com.multimif.controller;
 
-
-
 import com.multimif.compilation.Compile;
+import com.multimif.git.GitConstantes;
 import com.multimif.util.Constantes;
+import com.multimif.util.JsonUtil;
 import com.multimif.util.Status;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.multimif.util.JsonUtil;
-import javax.persistence.EntityManager;
 
 /**
  * Created by Mahmoud on 15/11/2016.
  */
 @RestController
-@RequestMapping("/compile/{branch}")
+@RequestMapping("/compile/{idCurrentUserStr}/{idProjectStr}/{branch}")
 public class CompileController {
 
-    private EntityManager entityManager;
-
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json; charset=utf-8")
-    public
+    @RequestMapping(method = RequestMethod.GET, produces = GitConstantes.APPLICATION_JSON_UTF8)
     @ResponseBody
-    ResponseEntity<String> compile(@RequestParam("idProject") long idProject,@RequestParam("idCurrentUser") Long idCurrentUser,@PathVariable String branch) {
+    public ResponseEntity<String> compile(@PathVariable String idProjectStr,
+                                          @PathVariable String idCurrentUserStr,
+                                          @PathVariable String branch) {
+        Long idProject = Long.valueOf(idProjectStr);
+        Long idCurrentUser = Long.valueOf(idCurrentUserStr);
 
         System.out.println("compile");
-        Compile compile=new Compile();
-        String Resultat ="erreur";
+        Compile compile = new Compile();
+        String resultat = "erreur";
+
         try {
-        Resultat   = compile.executeCompilation(idProject,idCurrentUser,branch);
+            resultat = compile.executeCompilation(idProject,idCurrentUser,branch);
         } catch (Exception ex) {
             return new ResponseEntity<String>(JsonUtil.convertToJson(new Status(Constantes.OPERATION_CODE_RATE,
                     ex.getMessage())), HttpStatus.NOT_FOUND);
         }
 
-        return new ResponseEntity<String>(JsonUtil.convertToJson(Resultat), HttpStatus.OK);
+        return new ResponseEntity<String>(JsonUtil.convertToJson(resultat), HttpStatus.OK);
     }
-
 }
