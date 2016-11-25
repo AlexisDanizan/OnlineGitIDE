925e4a45573af2b74dc439f649ea22471e49b7dc
Nicolas Giacometti <Nicolas Giacometti@localhost>
2016-11-25 17:42:20 +0100

    modif getArborescence : prise en compte des TemporaryFiles

---
+++ Application/src/main/java/webapp/ressources/JS/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    /* Connexion à l'application */
+    $("#btnConnexion").on("click",function(e){
+        e.preventDefault();
+        var url = "/api/user/login?" + $(this).closest("form").serialize();
+        ApiRequest('GET',url,"",connexion);
+    });
+
+    /* Inscription à l'application */
+    $("#btnInscription").on("click",function(e){
+        e.preventDefault();
+        var url = "/api/user/?" + $(this).closest("form").serialize();
+        ApiRequest('POST',url,"",connexion);
+    });
+
+});
