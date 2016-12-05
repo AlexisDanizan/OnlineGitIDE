86c9d00761e2c23f1341f059744834156afc7308
p1307873 <p1307873@localhost>
2016-12-05 17:06:29 +0100

    archive + compile ajax

--- Application/src/main/webapp/ressources/JS/editAjax.js
+++ Application/src/main/webapp/ressources/JS/editAjax.js
@@ -101,6 +101,7 @@ function compiler(idCreator,idProject,branch){
         js.type = "text/javascript";
         js.src = json["src"];
         js.setAttribute("data-autoplay","true");
+        js.id = "asciicast-" + json["id"];
         document.getElementById("contenuCompilation").appendChild(js);
     });
 }
