9ff758f17bf5cdcc641a8b4340f39b44f39d3351
p1307873 <p1307873@localhost>
2016-12-06 23:04:35 +0100

    Diff correction

--- Application/src/main/webapp/ressources/JS/home.js
+++ Application/src/main/webapp/ressources/JS/home.js
@@ -352,7 +352,7 @@ function getDate(timestamp){
     var date = new Date(timestamp/1000);
     console.log(timestamp/1000);
     var hours = date.getHours();
-    var minutes = "0" + date.getMinutes();
-    var seconds = "0" + date.getSeconds();
-   return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+   return hours + ':' + minutes + ':' + seconds;
 }
