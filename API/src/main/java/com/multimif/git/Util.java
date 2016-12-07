7b9ad0e545606600a3eb13934b6ec6e49bc91955
alexisdanizan <alexis.danizan@gmail.com>
2016-12-07 10:30:50 +0100

    Petite couleur

--- API/src/main/java/com/multimif/git/Util.java
+++ API/src/main/java/com/multimif/git/Util.java
@@ -160,9 +160,10 @@ public class Util {
         // prepare a new folder for the cloned repository
         String path = getGitRepo(creator, repository);
         System.out.println("path: " + path);
-        path = "/root/inf1096m-2016-grp01/repositories/test/";
+        //path = "/root/inf1096m-2016-grp01/repositories/test/";
         File localPath = new File(path);
         System.out.println("sqdsq");
+        remoteURL = "https://github.com/hadjiszs/Interpolation.git";
         // then clone
         try {
             /*Git.cloneRepository().setDirectory(localPath)
